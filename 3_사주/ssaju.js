f29454d2a48c08a41ca816f4f23f20f4fc13831a
Xuswnss <cooconut0226@gmail.com>
2026-05-11 16:18:49 +0900

    feat : ssaju install

---
+++ 3_사주/ssaju.js
@@ -0,0 +1,12 @@
+// import { calculateSaju } from "ssaju";
+
+// const result = calculateSaju({
+//   year: 0, month: 3, day: 16,
+//   hour: 0, minute: 0,
+//   gender: "",
+// });
+// name = ''
+// name =""
+// console.log('###', name, '사주#####')
+// console.log(result.toCompact());
+// console.log(result.toMarkdown());
